caf2c9cdfe37f1be9d6afeb04085d4b7ea86a36e
zach <zach@dylibso.com>
2024-04-30 16:29:46 -0700

    refactor: add reason argument to assert import, add macros

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ mod harness {
     extern "C" {
         pub fn call(name: u64, input: u64) -> u64;
         pub fn time(name: u64, input: u64) -> u64;
-        pub fn assert(name: u64, value: u64);
+        pub fn assert(name: u64, value: u64, message: u64);
         pub fn reset();
         pub fn group(name: u64);
     }
@@ -56,23 +56,34 @@ pub fn time_sec(func_name: impl AsRef<str>, input: impl ToMemory) -> Result<f64>
     time_ns(func_name, input).map(|x| x as f64 / 1e9)
 }
 
-/// Assert that the `outcome` is true, naming the assertion with `msg`, which will be used as a label in the CLI runner.
-pub fn assert(msg: impl AsRef<str>, outcome: bool) {
-    let msg_mem = Memory::from_bytes(msg.as_ref()).expect("assert message Extism memory");
+/// Assert that the `outcome` is true, naming the assertion with `name`, which will be used as a label in the CLI runner. The `reason` argument
+/// will be used to print a message when the assertion fails, this should contain some additional information about values being compared.
+pub fn assert(name: impl AsRef<str>, outcome: bool, reason: impl AsRef<str>) {
+    let name_mem = Memory::from_bytes(name.as_ref()).expect("assert name Extism memory");
+    let reason_mem = Memory::from_bytes(reason.as_ref()).expect("assert reason Extism memory");
     unsafe {
-        harness::assert(msg_mem.offset(), outcome as u64);
+        harness::assert(name_mem.offset(), outcome as u64, reason_mem.offset());
     }
-    msg_mem.free();
+    reason_mem.free();
+    name_mem.free();
 }
 
 /// Assert that `x` and `y` are equal, naming the assertion with `msg`, which will be used as a label in the CLI runner.
-pub fn assert_eq<U, T: PartialEq<U>>(msg: impl AsRef<str>, x: T, y: U) {
-    assert(msg, x == y);
+pub fn assert_eq<U: std::fmt::Debug, T: std::fmt::Debug + PartialEq<U>>(
+    msg: impl AsRef<str>,
+    x: T,
+    y: U,
+) {
+    assert(msg, x == y, format!("Expected {:?} == {:?}", x, y));
 }
 
 /// Assert that `x` and `y` are not equal, naming the assertion with `msg`, which will be used as a label in the CLI runner.
-pub fn assert_ne<U, T: PartialEq<U>>(msg: impl AsRef<str>, x: T, y: U) {
-    assert(msg, x != y);
+pub fn assert_ne<U: std::fmt::Debug, T: std::fmt::Debug + PartialEq<U>>(
+    msg: impl AsRef<str>,
+    x: T,
+    y: U,
+) {
+    assert(msg, x != y, format!("Expected {:?} != {:?}", x, y));
 }
 
 // Create a new test group. NOTE: these cannot be nested and starting a new group will end the last one.
@@ -109,4 +120,45 @@ pub fn group(name: impl AsRef<str>, f: impl FnOnce() -> Result<()>) -> Result<()
     let res = f();
     reset();
     res
+}
+
+#[macro_export]
+macro_rules! assert {
+    ($name:expr, $b:expr) => {
+        $crate::assert($name, $b, "Assertion failed ({}, line {})" file!(), line!());
+    }
+}
+
+#[macro_export]
+macro_rules! assert_eq {
+    ($name:expr, $a:expr, $b:expr) => {
+        $crate::assert(
+            $name,
+            $a == $b,
+            format!(
+                "Expected {:?} == {:?} ({}, line {})",
+                $a,
+                $b,
+                file!(),
+                line!()
+            ),
+        );
+    };
+}
+
+#[macro_export]
+macro_rules! assert_ne {
+    ($name:expr, $a:expr, $b:expr) => {
+        $crate::assert(
+            $name,
+            $a != $b,
+            format!(
+                "Expected {:?} != {:?} ({}, line {})",
+                $a,
+                $b,
+                file!(),
+                line!()
+            ),
+        );
+    };
 }
